b54fb2894af02a4300e5d2833dd2cb7e4656e32d
Hwanseung Lee <hs1217.lee@gmail.com>
2017-08-31 08:51:33 +0900

    Introduce macha which is test framework for node.js. (#27)
    Mocha is a simple, flexible, fun JavaScript test framework for node.js and
    the browser.
    
    ISSUE=none

--- examples/calculator.js
+++ examples/calculator.js
@@ -14,40 +14,53 @@
  * limitations under the License.
  */
 const assert = require('assert');
-
 var calculator = require('bindings')('bacardi.node')
-assert.equal(calculator.add(1, 2), 3);
-assert.equal(calculator.add(3.3, 4.3), 7.6);
-assert.equal(calculator.add(3.3, -4.3), -1);
-assert.equal(calculator.add(-3.3, 4.3), 1);
-assert.equal(calculator.add(1.2, 0), 1.2);
-assert.equal(calculator.add(4.5878, 1.1234), 5.7112);
 
-assert.equal(calculator.sub(1, 2), -1);
-assert.equal(calculator.sub(3.3, 4.3), -1);
-assert.equal(calculator.sub(3.3, -4.3), 7.6);
-assert.equal(calculator.sub(-3.3, 4.3), -7.6);
-assert.equal(calculator.sub(1.2, 0), 1.2);
-// TODO(chivalryko): return wrong value
-// Please see : https://github.com/lunchclass/bacardi/issues/18
-// assert.equal(calculator.sub(4.5878, 1.1234), 3.4644);
-
-assert.equal(calculator.mul(1, 2), 2);
-assert.equal(calculator.mul(3.3, 4.3), 14.19);
-assert.equal(calculator.mul(3.3, -4.3), -14.19);
-assert.equal(calculator.mul(-3.3, 4.3), -14.19);
-assert.equal(calculator.mul(1.2, 0), 0);
-// TODO(chivalryko): return wrong value
-// Please see : https://github.com/lunchclass/bacardi/issues/18
-// assert.equal(calculator.mul(4.5878, 1.1234), 5.15393452);
-
-assert.equal(calculator.div(1, 2), 0.5);
-// TODO(chivalryko): return wrong value
-// Please see : https://github.com/lunchclass/bacardi/issues/18
-// assert.equal(calculator.div(3.3, 4.3), 0.76744186);
-// assert.equal(calculator.div(3.3, -4.3), -0.76744186);
-// assert.equal(calculator.div(-3.3, 4.3), -0.76744186);
-assert.equal(calculator.div(1.2, 0), 0);
-// TODO(chivalryko): return wrong value
-// Please see : https://github.com/lunchclass/bacardi/issues/18
-// assert.equal(calculator.div(4.5878, 1.1234), 0.767442);
+describe('add function test', function() {
+  it('should be equal with expected value', function() {
+    assert.equal(calculator.add(1, 2), 3);
+    assert.equal(calculator.add(3.3, 4.3), 7.6);
+    assert.equal(calculator.add(3.3, -4.3), -1);
+    assert.equal(calculator.add(-3.3, 4.3), 1);
+    assert.equal(calculator.add(1.2, 0), 1.2);
+    assert.equal(calculator.add(4.5878, 1.1234), 5.7112);
+  });
+});
+describe('sub function test', function() {
+  it('should be equal with expected value', function() {
+    assert.equal(calculator.sub(1, 2), -1);
+    assert.equal(calculator.sub(3.3, 4.3), -1);
+    assert.equal(calculator.sub(3.3, -4.3), 7.6);
+    assert.equal(calculator.sub(-3.3, 4.3), -7.6);
+    assert.equal(calculator.sub(1.2, 0), 1.2);
+    // TODO(chivalryko): return wrong value
+    // Please see : https://github.com/lunchclass/bacardi/issues/18
+    // assert.equal(calculator.sub(4.5878, 1.1234), 3.4644);
+  });
+});
+describe('mul function test', function() {
+  it('should be equal with expected value', function() {
+    assert.equal(calculator.mul(1, 2), 2);
+    assert.equal(calculator.mul(3.3, 4.3), 14.19);
+    assert.equal(calculator.mul(3.3, -4.3), -14.19);
+    assert.equal(calculator.mul(-3.3, 4.3), -14.19);
+    assert.equal(calculator.mul(1.2, 0), 0);
+    // TODO(chivalryko): return wrong value
+    // Please see : https://github.com/lunchclass/bacardi/issues/18
+    // assert.equal(calculator.mul(4.5878, 1.1234), 5.15393452);
+  });
+});
+describe('div function test', function() {
+  it('should be equal with expected value', function() {
+    assert.equal(calculator.div(1, 2), 0.5);
+    // TODO(chivalryko): return wrong value
+    // Please see : https://github.com/lunchclass/bacardi/issues/18
+    // assert.equal(calculator.div(3.3, 4.3), 0.76744186);
+    // assert.equal(calculator.div(3.3, -4.3), -0.76744186);
+    // assert.equal(calculator.div(-3.3, 4.3), -0.76744186);
+    assert.equal(calculator.div(1.2, 0), 0);
+    // TODO(chivalryko): return wrong value
+    // Please see : https://github.com/lunchclass/bacardi/issues/18
+    // assert.equal(calculator.div(4.5878, 1.1234), 0.767442);
+  });
+});
